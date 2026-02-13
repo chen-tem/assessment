440b9acae01fe7198ddb43aed5966676416ae22f
chen-tem <j.chenthuri@gmail.com>
2026-02-13 23:45:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IAssessmentService, AssessmentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +23,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
